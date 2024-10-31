ea942431f4da1bd13198a37d79df36a30744b8f3
Agus Solihin <bagussajiwo305@gmail.com>
2024-10-31 23:07:09 +0700

    style nafbar

--- js/script.js
+++ js/script.js
@@ -8,3 +8,15 @@ document.querySelector("#humbuger-menu").onclick = () => {
     document.querySelector("#navactive").id = "";
   }
 };
+
+const hamburger = document.querySelector("#humbuger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    const navnav = document.querySelector("#navactive");
+    if (navnav == undefined) {
+      navbarNav.id = "";
+    } else {
+      document.querySelector("#navactive").id = "";
+    }
+  }
+});
